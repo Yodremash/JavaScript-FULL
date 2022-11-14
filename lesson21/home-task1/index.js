a8c881d374093d28315e18fb3dc2740d60395571
Alexey <bumbumbu2@gmail.com>
2022-11-14 10:18:32 +0200

    init files

---
+++ lesson21/home-task1/index.js
@@ -0,0 +1,11 @@
+function getTitleElement() {
+  const titleElem = document.querySelector('.title');
+  return console.dir(titleElem);
+}
+function getInputElement() {
+  const inputElem = document.querySelector('input[type= "text"]');
+  return console.dir(inputElem);
+}
+
+getTitleElement();
+getInputElement();
