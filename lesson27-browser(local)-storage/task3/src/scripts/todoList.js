ec10ead3795ba6c44a63242807292eda615bf26d
Alexey <bumbumbu2@gmail.com>
2022-11-26 14:38:12 +0200

    fix

--- lesson27-browser(local)-storage/task3/src/scripts/todoList.js
+++ lesson27-browser(local)-storage/task3/src/scripts/todoList.js
@@ -4,7 +4,6 @@ import { onToggleTask } from './updateTask.js';
 export const initTodoListHandlers = () => {
   const createBtnElem = document.querySelector('.create-task-btn');
   createBtnElem.addEventListener('click', onCreateTask);
+  const todoListElem = document.querySelector('.list');
+  todoListElem.addEventListener('click', onToggleTask);
 };
-
-const todoListElem = document.querySelector('.list');
-todoListElem.addEventListener('click', onToggleTask);
